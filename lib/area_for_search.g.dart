9c817040541c046f9611d14a2a4892af6950bd55
dkitagawa <kitagawa@hituji.jp>
2024-11-15 14:48:01 +0900

    riverpodの生成ファイルもコミットに追加することにした。

---
+++ lib/area_for_search.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'area_for_search.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$areaForSearchHash() => r'c7cef32c6fb3be30d6d1b45ce9013d7ade742dc7';
+
+/// See also [AreaForSearch].
+@ProviderFor(AreaForSearch)
+final areaForSearchProvider =
+    AutoDisposeNotifierProvider<AreaForSearch, String>.internal(
+  AreaForSearch.new,
+  name: r'areaForSearchProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$areaForSearchHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AreaForSearch = AutoDisposeNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/date_for_search.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'date_for_search.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dateForSearchHash() => r'bedafef3183a0fe6091784afdda81aa62d48e753';
+
+/// See also [DateForSearch].
+@ProviderFor(DateForSearch)
+final dateForSearchProvider =
+    AutoDisposeNotifierProvider<DateForSearch, DateTime>.internal(
+  DateForSearch.new,
+  name: r'dateForSearchProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dateForSearchHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DateForSearch = AutoDisposeNotifier<DateTime>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
