fc3043b47fff522f32364eed0345ec67235a1626
dkitagawa <kitagawa@hituji.jp>
2024-11-30 14:13:37 +0900

    loadWeatherImageを_getWeatherImageUrlに変更した

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,7 @@ class InputColumn extends ConsumerWidget {
     final String areaForSearch = ref.watch(areaForSearchProvider);
     final DateTime dateForSearch = ref.watch(dateForSearchProvider);
 
-    loadWeatherImage(areaForSearch, dateForSearch, ref);
+    _getWeatherImageUrl(areaForSearch, dateForSearch, ref);
     _areaController.text = areaForSearch; //riverpod初期値の登録
 
     return Column(
@@ -182,7 +182,7 @@ class InputColumn extends ConsumerWidget {
                 }
 
                 providerNotifier.clear();
-                loadWeatherImage(areaForSearch, dateForSearch, ref);
+                _getWeatherImageUrl(areaForSearch, dateForSearch, ref);
               },
               child: const Icon(
                 Icons.camera_alt,
@@ -206,7 +206,7 @@ class InputColumn extends ConsumerWidget {
     }
   }
 
-  void loadWeatherImage(String area, DateTime date, WidgetRef ref)  {
+  void _getWeatherImageUrl(String area, DateTime date, WidgetRef ref)  {
     var prompt = "指定された地域の、指定された日付の天気予報を表現する画像を生成してください。【コンセプト】空撮ではなく地上に立つ人間の視点で対象地域を天気予報の通りの状態で描きます。対象地域のシンボリックな建物・名産品・名物・人間を盛り込み、マンガか映画の有名なシーンを大胆にオマージュしてください。人物描写が印象的だと良いです。【各情報の表示サイズ】情報の表示サイズは以下の順：地域名>>日付（MM/dd形式に変換して表示）>>>>>>>>>>>>対象日付の最高／最低気温と降水確率";
 
     prompt = "対象地域：$area。対象日付：$date。$prompt";
