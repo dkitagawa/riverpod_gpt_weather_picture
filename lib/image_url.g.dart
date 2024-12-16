71872f596de4c1b523170bf4f69f040ce944853c
dkitagawa <kitagawa@hituji.jp>
2024-12-16 15:22:56 +0900

    image_url.dartのコミット漏れだったため追加

---
+++ lib/image_url.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'image_url.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$imageUrlHash() => r'b090a4bd6e583d1f19e40cc9bd95b285b293bde0';
+
+/// See also [ImageUrl].
+@ProviderFor(ImageUrl)
+final imageUrlProvider =
+    AutoDisposeAsyncNotifierProvider<ImageUrl, String>.internal(
+  ImageUrl.new,
+  name: r'imageUrlProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$imageUrlHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ImageUrl = AutoDisposeAsyncNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
