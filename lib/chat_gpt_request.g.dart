1d4810d31be20a3660433eefb3e9214b2ffc1d3f
dkitagawa <kitagawa@hituji.jp>
2024-12-28 15:35:21 +0900

    riverpodの自動生成ファイルを追加（chatGPT用）

---
+++ lib/chat_gpt_request.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chat_gpt_request.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$chatGPTRequestHash() => r'2b7002fee39014b208c8c6e6de671b88eb9c87f9';
+
+/// See also [ChatGPTRequest].
+@ProviderFor(ChatGPTRequest)
+final chatGPTRequestProvider =
+    AutoDisposeAsyncNotifierProvider<ChatGPTRequest, String>.internal(
+  ChatGPTRequest.new,
+  name: r'chatGPTRequestProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$chatGPTRequestHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ChatGPTRequest = AutoDisposeAsyncNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
